08919e253ebff93f18417ab65e380d3d0afbc2b7
procha <pedrojrocha@outlook.com>
2021-11-09 21:25:11 +0100

    Update object.py with pylint feedback

--- image_analysis/objects.py
+++ image_analysis/objects.py
@@ -25,3 +25,23 @@ class ObjectClass:
     def __del__(self):
         '''Class destructor'''
         return None
+
+    def get_x (self):
+        '''Return x coordinate of the object'''
+        return self.x_coord
+
+    def get_y (self):
+        '''Return y coordinate of the object'''
+        return self.y_coord
+
+    def get_w (self):
+        '''Return the width of the object'''
+        return self.width
+
+    def get_h (self):
+        '''Return the height of the object'''
+        return self.height
+
+    def get_text (self):
+        '''Return the text description of the object'''
+        return self.text
